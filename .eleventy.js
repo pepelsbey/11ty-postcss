93d45456ceda6de9730316e54bf6108ecdf546a2
Vadim Makeev <pepelsbey@gmail.com>
2022-05-15 20:50:46 +0200

    Init

---
+++ .eleventy.js
@@ -0,0 +1,41 @@
+const postcss = require('postcss');
+const autoprefixer = require('autoprefixer');
+const csso = require('postcss-csso');
+const pimport = require('postcss-import');
+
+module.exports = (config) => {
+  config.addExtension('css', {
+    outputFileExtension: 'css',
+    compile: async (inputContent, inputPath) => {
+      if (inputPath.endsWith('.css')) {
+        let output = await postcss([
+          pimport,
+          autoprefixer,
+          csso
+        ]).process(inputContent, { from: inputPath });
+
+        return async () => {
+          return output.css;
+        }
+      }
+    }
+  });
+
+  // Config
+
+  return {
+    dir: {
+      input: 'src',
+      output: 'dist',
+      includes: 'includes',
+      layouts: 'layouts',
+      data: 'data'
+    },
+    dataTemplateEngine: 'njk',
+    markdownTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    templateFormats: [
+      'md', 'njk', 'css',
+    ],
+  };
+};
